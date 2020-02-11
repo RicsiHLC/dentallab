8450e704455746d351c89b5a98ad2e681f3b4376
Szabo Richard <richard.szabo@healcloud.com>
2020-02-11 20:12:23 +0100

    logo, base header component, scss preprocessor added

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import logo from '../../asstes/images/main_logo.png' 
+
+const Header = () => <header className="App-header"><img alt="logo" src={logo} /></header>;
+
+export default Header;
